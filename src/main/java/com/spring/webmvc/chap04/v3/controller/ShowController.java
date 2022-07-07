8b6ef937e2be577897a9ccd339729e6dab6a094a
KRDJK <kdf556@naver.com>
2022-07-07 18:18:27 +0900

    [22.07.07] MVC 패턴, 프론트 컨트롤러 생성 및 리팩토링 체험

--- src/main/java/com/spring/webmvc/chap04/v3/controller/ShowController.java
+++ src/main/java/com/spring/webmvc/chap04/v3/controller/ShowController.java
@@ -19,11 +19,9 @@ public class ShowController implements ControllerV3 {
         List<Member> members = repository.findAll();
 
         // Model : Controller 와 view 사이의 데이터(여기선 리스트:members 데이터가 필요함)를 운반하는 수단 객체
-        // - 여기서 모델의 역할은 Request 객체가 담당
-
-
         // 이름은 내 맘대로 정함 (key값이 됨.)
 //        request.setAttribute("mList", members); // request 객체에 데이터를 저장
+//                                                  mList라는 이름을 가진 멤버 리스트를 전달
 
 
         return new View("/WEB-INF/views/members.jsp");
